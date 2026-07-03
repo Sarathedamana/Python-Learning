4f831d3b0ccde9473aa8e5b5676b9e1759964613
SarathV <sarathedamana@gmail.com>
2026-07-03 18:49:02 +0530

    Added Week 1 python exercises

---
+++ week 1/aboutme.py
@@ -0,0 +1,4 @@
+print("My name is Sarath")
+print("I am from Pataliputra")
+print("I am working as a support enginner")
+print("I love to travel")
---
+++ week 1/hello.py
@@ -0,0 +1,4 @@
+print('Hello World')
+print('My name is Sarath')
+print('I am learning python now')
+print('my goal is to become an AI engineer')
