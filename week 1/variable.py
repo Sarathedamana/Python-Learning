dc6f50d5973121048a8cee7fe82c4a637fb91323
SarathV <sarathedamana@gmail.com>
2026-07-04 23:26:08 +0530

    Added Variable Practice

---
+++ week 1/variable.py
@@ -0,0 +1,19 @@
+Name = "Sarath"
+Age = 36
+Department = "Application Support Engineer"
+Experience =12.5
+Monthly_Salary = 100000
+Salary_hike = 10/100
+New_Salary= Monthly_Salary * Salary_hike + Monthly_Salary
+print("Name is",Name)
+print("Current Age is", Age)
+print("Department name is", Department)
+print("Years of Experience", Experience)
+print("Monthly salary is",Monthly_Salary)
+print("New salary is",New_Salary)
+print(type(Name))
+print(type(Age))
+print(type(Department))
+print(type(Experience))
+print(type(Monthly_Salary))
+print(type(New_Salary))
