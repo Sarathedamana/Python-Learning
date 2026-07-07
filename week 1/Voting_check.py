2c4f4ba3cdfd03ebc83762240a715583b8045060
SarathV <sarathedamana@gmail.com>
2026-07-07 18:49:36 +0530

    If and Comparison operator

---
+++ week 1/Voting_check.py
@@ -0,0 +1,6 @@
+Age = float(input("Enter your age"))
+if Age >= 18:
+    print("you are eligible to Vote")
+if Age <18 :
+    print("You are not eligible to Vote")
+print("Program Finished")       
---
+++ week 1/temp.py
@@ -0,0 +1,6 @@
+temperature=float(input("Enter the temperature :"))
+if temperature >30:
+    print("It is sunny day")
+if temperature < 30 :
+    print("Cold weather, wear the jacket")    
+print("program finished")    
