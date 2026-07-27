631d7fc3b6d6bb2309ca522b16f1ae734bfb8255
SarathV <sarathedamana@gmail.com>
2026-07-27 15:23:39 +0530

    While loop assignments

---
+++ Week 3/multiply.py
@@ -0,0 +1,7 @@
+number=int(input("Enter the number:"))
+count=1
+while count <=10:
+    print("number",number,"count",count, "output:",number*count)
+    count=count+1
+    
+
---
+++ Week 3/odd_num.py
@@ -0,0 +1,6 @@
+count=1
+while count<=20:
+    print(count)
+    count=count+2
+    
+
